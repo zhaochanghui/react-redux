296bf5669516a8a4c4ec8322e334fcc88ebcece4
zhaochanghui <2636190619@qq.com>
2017-10-25 19:22:46 +0800

    下班
    下班

--- app/index.js
+++ app/index.js
@@ -1,107 +1,20 @@
-var Redux = require('redux');
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { connect, Provider } from 'react-redux';
 
-//action creator
-var addTodo = function(text){
-	return {
-		type:"add_todo",
-		text: text
-	};
-};
+//http://blog.csdn.net/q1056843325/article/details/54880804
 
-//reducers
-var todoReducer = function(state,action){
-	if(typeof state === 'undefined'){
-		return [];
-	}
-
-	switch(action.type){
-		case 'add_todo':
-			return state.slice(0).concat({
-				text: action.text,
-				completed: false
-			});
-			break;
-		default:
-			return state;	
-	}
-};
-
-var store = Redux.createStore(todoReducer);
-
-
-
-class MyApp extends React.Component{
-	constructor(props) {
-			super(props);
-			this.handleAdd = this.handleAdd.bind(this);
-			this.onChange = this.onChange.bind(this);
-			this.state = {items: store.getState()};
-		}
-
-	componentDidMount() {
-				var unsubscribe = store.subscribe(this.onChange);
-			}
-	onChange() {
-		this.setState({items: store.getState()});
-	}
-
-	handleAdd() {
-		var input = ReactDOM.findDOMNode(this.refs.todo);
-		var value = input.value.trim();
-
-		if(value){
-			store.dispatch(addTodo(value));
-		}
-
-		input.value = '';
-	}
-
-	*show(){
-		yield 'aa';
-		yield 'bb';
-		yield 'cc';
-		return 'xx';
-	}
-
+class Counter extends Component{
 	render() {
-		console.log(this.show().next());
-
+		const { value,reducerHandler, addHandler } = this.props;
 		return (
 			<div>
-				<input ref='todo' placeholder='输入tudo..' style={{marginRight: "15px"}} />
-				<button onClick={this.handleAdd}>点击添加</button>
-				<ul>
-					{this.state.items.map(function(item,key){
-						return <li>{item.text}</li>;
-					})}
-				</ul>
-
-				<div>
-					
-				</div>
+				<p>{value} </p>
+				<button onClick={reducerHandler}>-</button>
+				<button onClick={addHandler}>+</button>
 			</div>
 			);
-	}				
-}
-
-
-
-class App extends React.Component {
-	render() {
-		return (
-			<div>
-				<MyApp />
-			</div>
-		);	
 	}
 }
-
-
-
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-);
-
---
+++ dist/0.f7bda2e51d4e37bfd78d.hot-update.js
@@ -0,0 +1,244 @@
+webpackHotUpdate(0,[
+/* 0 */,
+/* 1 */
+false,
+/* 2 */
+false,
+/* 3 */,
+/* 4 */
+false,
+/* 5 */
+false,
+/* 6 */
+false,
+/* 7 */,
+/* 8 */
+false,
+/* 9 */
+false,
+/* 10 */
+false,
+/* 11 */
+false,
+/* 12 */
+false,
+/* 13 */
+false,
+/* 14 */
+false,
+/* 15 */,
+/* 16 */
+false,
+/* 17 */
+false,
+/* 18 */,
+/* 19 */,
+/* 20 */
+false,
+/* 21 */
+false,
+/* 22 */
+false,
+/* 23 */
+false,
+/* 24 */
+false,
+/* 25 */
+false,
+/* 26 */
+false,
+/* 27 */
+false,
+/* 28 */
+false,
+/* 29 */
+false,
+/* 30 */
+false,
+/* 31 */
+false,
+/* 32 */
+false,
+/* 33 */
+false,
+/* 34 */,
+/* 35 */,
+/* 36 */,
+/* 37 */,
+/* 38 */
+false,
+/* 39 */
+false,
+/* 40 */
+false,
+/* 41 */
+false,
+/* 42 */
+false,
+/* 43 */
+false,
+/* 44 */
+false,
+/* 45 */
+false,
+/* 46 */
+false,
+/* 47 */
+false,
+/* 48 */
+false,
+/* 49 */
+false,
+/* 50 */
+false,
+/* 51 */
+false,
+/* 52 */
+false,
+/* 53 */
+false,
+/* 54 */
+false,
+/* 55 */
+false,
+/* 56 */
+false,
+/* 57 */,
+/* 58 */,
+/* 59 */,
+/* 60 */,
+/* 61 */,
+/* 62 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\nvar _react = __webpack_require__(34);\n\nvar _react2 = _interopRequireDefault(_react);\n\nvar _reactDom = __webpack_require__(128);\n\nvar _reactDom2 = _interopRequireDefault(_reactDom);\n\nfunction _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hcHAvaW5kZXguanM/ZGFkYyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBOzs7O0FBRUEiLCJmaWxlIjoiNjIuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgUmVhY3QgZnJvbSAncmVhY3QnO1xyXG5pbXBvcnQgeyBDb21wb25lbnQgfSBmcm9tICdyZWFjdCc7XHJcbmltcG9ydCBSZWFjdERPTSBmcm9tICdyZWFjdC1kb20nO1xyXG5cblxuXG4vLyBXRUJQQUNLIEZPT1RFUiAvL1xuLy8gLi9hcHAvaW5kZXguanMiXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///62\n");
+
+/***/ }),
+/* 63 */
+false,
+/* 64 */
+false,
+/* 65 */
+false,
+/* 66 */
+false,
+/* 67 */
+false,
+/* 68 */
+false,
+/* 69 */
+false,
+/* 70 */
+false,
+/* 71 */
+false,
+/* 72 */
+false,
+/* 73 */
+false,
+/* 74 */
+false,
+/* 75 */
+false,
+/* 76 */
+false,
+/* 77 */
+false,
+/* 78 */
+false,
+/* 79 */
+false,
+/* 80 */
+false,
+/* 81 */
+false,
+/* 82 */
+false,
+/* 83 */
+false,
+/* 84 */
+false,
+/* 85 */
+false,
+/* 86 */
+false,
+/* 87 */
+false,
+/* 88 */
+false,
+/* 89 */
+false,
+/* 90 */
+false,
+/* 91 */
+false,
+/* 92 */
+false,
+/* 93 */
+false,
+/* 94 */
+false,
+/* 95 */
+false,
+/* 96 */
+false,
+/* 97 */
+false,
+/* 98 */
+false,
+/* 99 */
+false,
+/* 100 */
+false,
+/* 101 */
+false,
+/* 102 */
+false,
+/* 103 */
+false,
+/* 104 */
+false,
+/* 105 */
+false,
+/* 106 */
+false,
+/* 107 */
+false,
+/* 108 */
+false,
+/* 109 */
+false,
+/* 110 */
+false,
+/* 111 */
+false,
+/* 112 */
+false,
+/* 113 */
+false,
+/* 114 */
+false,
+/* 115 */
+false,
+/* 116 */
+false,
+/* 117 */
+false,
+/* 118 */
+false,
+/* 119 */
+false,
+/* 120 */
+false,
+/* 121 */
+false,
+/* 122 */
+false,
+/* 123 */
+false,
+/* 124 */
+false,
+/* 125 */
+false
+])
